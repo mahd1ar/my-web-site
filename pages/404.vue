ba19ee370862cb9e322602adc88d15c188bf3361
mahd1ar <a.mahdiyar7@yahoo.com>
2025-11-10 22:38:37 +0330

    Just4testing n8n

--- pages/404.vue
+++ pages/404.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-black w-full">
         <img src="~/assets/gifs/pong-404.gif" alt="">
-        <h1 class="bg-white">page NOT FOUND </h1>
+        <h1 class="bg-white">Page NOT FOUND </h1>
         <nuxt-link class="bg-green-400 mt-10 font-chakra font-bold" to="/">
             &lt;
             &lt;
